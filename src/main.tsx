f24de9d39df8d8614d9da60d6c1102d21812be8a
houd8 <faraspmp@gmail.com>
2026-09-18 16:37:26 +0100

    feat: expose governed runtime controls

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "../App";
 import MissionControl from "./mission-control";
+import MissionRuntimeLauncher from "./mission-runtime-launcher";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <>
       <App />
       <MissionControl />
+      <MissionRuntimeLauncher />
     </>
   </React.StrictMode>
 );
